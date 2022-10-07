7dcd7067c6ffb1c14f9212b9bd7ca1ead0b1c10d
Abel DropDout <abeldzan@gmail.com>
2022-10-07 16:37:44 +0000

    feat: :lipstick: started v2

--- lib/remaths.dart
+++ lib/remaths.dart
@@ -1,7 +1,10 @@
 library remaths;
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
+import 'package:flutter/widgets.dart';
 import 'dart:math' as math;
 
 part 'core/_intervals/_interpolate.dart';
@@ -16,3 +19,10 @@ part 'core/_intervals/_spring.dart';
 part 'core/_intervals/_interpolateColors.dart';
 part 'core/measure.dart';
 part 'core/_intervals/_measure.dart';
+part 'v2/node.dart';
+part 'v2/core/shared/shared_value.dart';
+part 'v2/core/animation/timing.dart';
+part 'v2/core/animation/spring.dart';
+part 'v2/core/animation/delay.dart';
+
+part 'v2/core/animation/sequence.dart';
---
+++ lib/v2/core/animation/sequence.dart
@@ -0,0 +1,11 @@
+part of remaths;
+
+AnimationFunc withSequence2(List<AnimationFunc> animations) {
+  return (node) {
+    // animations.forEach((element) { })
+  };
+}
+
+_runAnimation(SharedValue node, AnimationFunc animation) {
+  //TODO
+}
